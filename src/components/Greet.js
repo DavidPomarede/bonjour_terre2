c8e7dd12e52372c513cb7bf878fb53ee94e85f6d
davidpom <davidpom@gmail.com>
2020-07-14 19:50:29 -0400

    first react steps2

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello David</h1>;
-}
+//function Greet() {
+//    return <h1>Hello David</h1>;
+//}
 
-export default Greet
+export const Greet = () => <h1> Hello David </h1>
+
+// this is a "named export" versus below, a "default export"
+
+
+//export default Greet
---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    // return (
+    //     <div>
+    //     <h1> Hello Vishwas</h1>
+    //     </div>
+    // )
+    return React.createElement('div', null, 'Hello Vishwas')
+}
+
+export default Hello
