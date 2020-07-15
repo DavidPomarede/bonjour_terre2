4b0b40fac4cf87a5130327cebd467019d61dc556
davidpom <davidpom@gmail.com>
2020-07-14 22:50:08 -0400

    setState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,8 +10,16 @@ class Counter extends Component {
         }
     }
 
-    increment() {
+    // Never change state directly, use setState() and because it's asynchronous,
+    // use a callback function because the console with display a different value 
+    // than the page.
 
+    increment() {
+        this.setState({
+            count:  this.state.count + 1
+        }, () => {console.log('Callback value', this.state.count)})
+
+        console.log(this.state.count)
     }
 
     render() {
